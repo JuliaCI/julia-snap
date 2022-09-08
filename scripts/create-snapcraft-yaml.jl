668d24ed1d0fae18099dcdef02866be2259a446e
Alex Arslan <ararslan@comcast.net>
2022-09-08 09:59:37 -0700

    Auto-generate the YAML file that describes the version (#11)
    Currently we have separate, almost entirely identical files,
    `snapcraft-lts.yaml` and `snapcraft-stable.yaml`, and on CI we copy the
    appropriate one to `snapcraft.yaml`. This requires keeping the patch
    versions of both LTS and stable up to date, as both are hard-coded in
    their respective files.
    
    Instead, we can automatically generate `snapcraft.yaml` based on the
    release type (`lts` or `stable`) and `versions.json`. This allows us to
    select the most recent LTS patch or stable version without needing to
    edit anything directly.
    
    Co-authored-by: Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>

---
+++ scripts/create-snapcraft-yaml.jl
@@ -0,0 +1,28 @@
+const CURRENT_LTS = v"1.6"  # keep this up to date
+
+#####
+
+if isempty(ARGS)
+    error("Expected release type as a command line argument to this script")
+end
+
+using HTTP
+using JSON3
+
+release = ARGS[1]
+contents = JSON3.read(HTTP.get("https://julialang-s3.julialang.org/bin/versions.json").body)
+
+versions = VersionNumber.(String.(keys(contents)))
+if release == "lts"
+    filter!(v -> v.major == CURRENT_LTS.major && v.minor == CURRENT_LTS.minor, versions)
+else
+    filter!(v -> v >= CURRENT_LTS, versions)
+end
+latest = maximum(versions)
+
+open(joinpath(dirname(@__DIR__), "snap", "snapcraft.yaml"), "w") do io
+    yaml = replace(read("snapcraft.yaml.in", String),
+                   raw"${JULIA_VERSION}" => string(latest),
+                   raw"${JULIA_SERIES}" => string(latest.major, '.', latest.minor))
+    print(io, yaml)
+end
