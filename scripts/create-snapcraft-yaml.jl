db7e680c4531dea700d1185db7176b4f33275bcb
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2022-09-08 09:19:19 -0500

    Use an input file as template

--- scripts/create-snapcraft-yaml.jl
+++ scripts/create-snapcraft-yaml.jl
@@ -9,12 +9,6 @@ end
 using HTTP
 using JSON3
 
-snaparch(jlarch) = jlarch == "x86_64" ? "amd64" :
-                   jlarch == "i686" ? "i386" :
-                   jlarch == "aarch64" ? "arm64" :
-                   jlarch == "armv7l" ? "armhf" :
-                   throw(ArgumentError("unrecognized architecture $jlarch"))
-
 release = ARGS[1]
 contents = JSON3.read(HTTP.get("https://julialang-s3.julialang.org/bin/versions.json").body)
 
@@ -26,48 +20,15 @@ else
 end
 latest = maximum(versions)
 
-urls = Dict(snaparch(file.arch) => file.url for file in contents[Symbol(latest)].files
-            if file.os == "linux" && !occursin("musl", file.triplet) &&
-               file.extension == "tar.gz")
+substitutions = Dict(
+    raw"${JULIA_VERSION}" => string(latest),
+    raw"${JULIA_SERIES}" => string(latest.major, '.', latest.minor)
+)
 
 open(joinpath(dirname(@__DIR__), "snap", "snapcraft.yaml"), "w") do io
-    print(io, """
-          name: julia
-          version: '$latest'
-          title: The Julia Language
-          summary: The Julia programming language
-          description: |
-            Julia is a high level, high performance, dynamic language for technical computing.
-          license: MIT
-
-          base: core18
-          confinement: classic
-          type: app
-
-          architectures:
-          """)
-    for arch in keys(urls)
-        println(io, "- build-on: ", arch)
+    yaml = read("snapcraft.yaml.in", String)
+    for pair in substitutions
+        yaml = replace(yaml, pair)
     end
-    print(io, """
-
-          apps:
-            julia:
-              command: bin/julia
-            julia-docs:
-              command: julia-docs.sh \$SNAP/share/doc/julia/html/en/index.html
-
-          parts:
-            julia:
-              plugin: dump
-              source:
-          """)
-    for (arch, url) in urls
-        println(io, "    - on ", arch, ": ", url)
-    end
-    print(io, """
-            julia-docs:
-              plugin: dump
-              source: snap/local
-          """)
+    print(io, yaml)
 end
